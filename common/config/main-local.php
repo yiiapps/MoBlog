3e1e1b5f01908df0cbad9b674c3f59d8d032686d
tangbq <tangbq@tiens.com>
2019-04-17 16:59:52 +0800

    update from dell msysgit

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -1,14 +1,14 @@
 <?php
 
-$config= [
+$config = [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=192.168.10.10;dbname=blog',
+            'dsn' => 'mysql:host=10.0.75.1;dbname=moblog',
             'username' => 'root',
-            'password' => '123456',
+            'password' => 'root',
             'charset' => 'utf8',
-            'tablePrefix'=>'moblog_',
+            'tablePrefix' => 'moblog_',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
@@ -24,16 +24,15 @@ if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
-        'class'=>'yii\debug\Module',
-        'allowedIPs'=>['192.168.*'],
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => ['192.168.*'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
-        'class'=>'yii\gii\Module',
-        'allowedIPs'=>['192.168.*'],
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['192.168.*'],
     ];
 }
-
 
 return $config;
